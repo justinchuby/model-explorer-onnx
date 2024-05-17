268bd3b5f37300164eee8732a3942814f446cd36
Justin Chu <justinchuby@users.noreply.github.com>
2024-05-17 09:47:39 -0700

    Better display

--- src/model_explorer_onnx/main.py
+++ src/model_explorer_onnx/main.py
@@ -9,6 +9,18 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+_TENSOR_DISPLAY_LIMIT = 1024
+
+
+def display_tensor(tensor: ir.TensorProtocol) -> str:
+    if tensor.size < _TENSOR_DISPLAY_LIMIT:
+        try:
+            return str(tensor.numpy())
+        except Exception as e:
+            logger.warning("Failed to display tensor: %s", e)
+            return str(tensor)
+    return str(tensor)
+
 
 def add_inputs_metadata(onnx_node: ir.Node, node: graph_builder.GraphNode):
     for i, input_value in enumerate(onnx_node.inputs):
@@ -25,14 +37,14 @@ def add_inputs_metadata(onnx_node: ir.Node, node: graph_builder.GraphNode):
 def add_outputs_metadata(onnx_node: ir.Node, node: graph_builder.GraphNode):
     for output in onnx_node.outputs:
         metadata = graph_builder.MetadataItem(id=str(output.index()), attrs=[])
-        dtype = str(output.dtype)
         type_str = str(output.type)
-        shape = str(output.shape)
+        shape_text = str(output.shape) if output.shape is not None else "?"
+
         metadata.attrs.append(
             graph_builder.KeyValue(key="__tensor_tag", value=output.name or "")
         )
         metadata.attrs.append(
-            graph_builder.KeyValue(key="tensor_shape", value=dtype + shape)
+            graph_builder.KeyValue(key="tensor_shape", value=f"{shape_text}")
         )
         metadata.attrs.append(graph_builder.KeyValue(key="type", value=type_str))
         node.outputsMetadata.append(metadata)
@@ -41,9 +53,11 @@ def add_outputs_metadata(onnx_node: ir.Node, node: graph_builder.GraphNode):
 def add_node_attrs(onnx_node: ir.Node, node: graph_builder.GraphNode):
     for attr in onnx_node.attributes.values():
         if isinstance(attr, ir.Attr):
-            node.attrs.append(
-                graph_builder.KeyValue(key=attr.name, value=str(attr.value))
-            )
+            if attr.type == ir.AttributeType.TENSOR:
+                attr_value = display_tensor(attr.value)
+            else:
+                attr_value = str(attr.value)
+            node.attrs.append(graph_builder.KeyValue(key=attr.name, value=attr_value))
         elif isinstance(attr, ir.RefAttr):
             node.attrs.append(
                 graph_builder.KeyValue(
@@ -64,7 +78,7 @@ def add_incoming_edges(
             # The input is a graph input. Create an input edge.
             node.incomingEdges.append(
                 graph_builder.IncomingEdge(
-                    sourceNodeId=input_value.name, # type: ignore
+                    sourceNodeId=input_value.name,  # type: ignore
                     sourceNodeOutputId=str(graph_inputs[input_value]),
                     targetNodeInputId=str(target_input_id),
                 )
@@ -98,15 +112,20 @@ def create_op_label(domain: str, op_type: str) -> str:
 
 
 def create_node(
-    onnx_node: ir.Node, graph_inputs: Mapping[ir.Value, int]
+    onnx_node: ir.Node, graph_inputs: Mapping[ir.Value, int], namespace: str = ""
 ) -> graph_builder.GraphNode | None:
     if onnx_node.name is None:
         logger.warning("Node does not have a name. Skipping node %s.", onnx_node)
         return None
+
+    embedded_namespace = onnx_node.name.split("/")[0:-1]
+    if embedded_namespace:
+        namespace = namespace + "/".join(embedded_namespace)
+    print(namespace)
     node = graph_builder.GraphNode(
         id=onnx_node.name,
         label=create_op_label(onnx_node.domain, onnx_node.op_type),
-        # namespace=None,
+        namespace=namespace,
     )
     add_incoming_edges(onnx_node, node, graph_inputs)
     add_node_attrs(onnx_node, node)
@@ -117,32 +136,82 @@ def create_node(
 
 def add_graph_io(graph: graph_builder.Graph, input_or_outputs: Sequence[ir.Value]):
     for value in input_or_outputs:
-        graph.nodes.append(
-            graph_builder.GraphNode(
-                id=value.name,  # type: ignore
-                label=value.name,  # type: ignore
+        node = graph_builder.GraphNode(
+            id=value.name,  # type: ignore
+            label=value.name,  # type: ignore
+        )
+        producer = value.producer()
+        if producer is not None and producer.name is not None:
+            node.incomingEdges.append(
+                graph_builder.IncomingEdge(
+                    sourceNodeId=producer.name,
+                    sourceNodeOutputId=str(value.index()),
+                    targetNodeInputId="0",
+                )
+            )
+
+        graph.nodes.append(node)
+
+
+def add_initializers(
+    graph: graph_builder.Graph, initializers: Sequence[ir.Value], namespace: str
+):
+    for initializer in initializers:
+        node = graph_builder.GraphNode(
+            id=initializer.name,  # type: ignore
+            label=initializer.name,  # type: ignore
+            namespace=namespace,
+        )
+        # Annotate the initializer node as an initializer
+        node.attrs.append(graph_builder.KeyValue(key="type", value="Initializer"))
+        # Add metadata for the output tensor
+        if initializer.const_value is None:
+            logger.warning(
+                "Initializer %s does not have a const value. Skipping.", initializer
+            )
+            graph.nodes.append(node)
+            continue
+        metadata = graph_builder.MetadataItem(id="0", attrs=[])
+        shape_text = (
+            str(initializer.const_value.shape)
+            if initializer.const_value.shape is not None
+            else "?"
+        )
+        metadata.attrs.append(
+            graph_builder.KeyValue(key="__tensor_tag", value=initializer.name or "")
+        )
+        metadata.attrs.append(
+            graph_builder.KeyValue(key="tensor_shape", value=f"{shape_text}")
+        )
+        metadata.attrs.append(
+            graph_builder.KeyValue(key="type", value=str(initializer.const_value.dtype))
+        )
+        metadata.attrs.append(
+            graph_builder.KeyValue(
+                key="value", value=display_tensor(initializer.const_value)
             )
         )
+        node.outputsMetadata.append(metadata)
+        graph.nodes.append(node)
 
 
-def create_graph(onnx_graph: ir.Graph | ir.Function) -> graph_builder.Graph:
-    graph = graph_builder.Graph(id="graph", nodes=[])
+def create_graph(onnx_graph: ir.Graph | ir.Function) -> graph_builder.Graph | None:
+    if onnx_graph.name is None:
+        logger.warning("Graph does not have a name. skipping graph: %s", onnx_graph)
+        return None
+    graph = graph_builder.Graph(id=onnx_graph.name or "graph", nodes=[])
     graph_inputs: dict[ir.Value, int] = {
         input_value: i for i, input_value in enumerate(onnx_graph.inputs)
     }
-    if isinstance(onnx_graph, ir.Graph):
-        graph_inputs.update(
-            {
-                input_value: i
-                for i, input_value in enumerate(onnx_graph.initializers.values())
-            }
-        )
     for onnx_node in onnx_graph:
-        node = create_node(onnx_node, graph_inputs)
+        node = create_node(onnx_node, graph_inputs, namespace=onnx_graph.name)
         if node is None:
             continue
         graph.nodes.append(node)
-    add_graph_io(graph, list(graph_inputs.keys()))
+    add_graph_io(graph, onnx_graph.inputs)
+    # Add initializers
+    if isinstance(onnx_graph, ir.Graph):
+        add_initializers(graph, list(onnx_graph.initializers.values()), onnx_graph.name)
     add_graph_io(graph, onnx_graph.outputs)
     return graph
 
@@ -159,9 +228,17 @@ class ONNXAdapter(model_explorer.Adapter):
     def convert(
         self, model_path: str, settings: dict[str, Any]
     ) -> model_explorer.ModelExplorerGraphs:
+        # TODO: Add shape inference and tensor display size as settings
         onnx_model = onnx.load(model_path)
+        onnx_model = onnx.shape_inference.infer_shapes(onnx_model)
         model = ir.serde.deserialize_model(onnx_model)
-        graphs = [create_graph(model.graph)]
+        main_graph = create_graph(model.graph)
+        graphs = []
+        main_graph = create_graph(model.graph)
+        assert main_graph is not None
+        graphs.append(main_graph)
         for function in model.functions.values():
-            graphs.append(create_graph(function))
+            function_graph = create_graph(function)
+            assert function_graph is not None
+            graphs.append(function_graph)
         return {"graphs": graphs}
