940f30ec9187c4650f1b2363238d2e7aae1d358c
Justin Chu <justinchuby@users.noreply.github.com>
2024-05-20 11:59:42 -0700

    Use the native __value field to display tensors (#26)
    ![image](https://github.com/justinchuby/model-explorer-onnx/assets/11205048/3ca28a9f-e813-4403-b544-44b411597563)

--- src/model_explorer_onnx/main.py
+++ src/model_explorer_onnx/main.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import json
 import logging
 from typing import Any, Literal, Sequence
 
@@ -12,31 +13,61 @@ from onnxscript import ir
 
 logger = logging.getLogger(__name__)
 
-_TENSOR_DISPLAY_LIMIT = 1024
 _DEFAULT_OPSET_VERSION = 18
 
 
-def display_tensor(tensor: ir.TensorProtocol | None) -> str:
+class Settings:
+    def __init__(self, const_element_count_limit: int = 1024, **_: Any):
+        self.const_element_count_limit: int = const_element_count_limit
+
+
+def _tensor_to_numpy(tensor: ir.TensorProtocol) -> np.ndarray:
+    array = tensor.numpy()
+    if tensor.dtype == ir.DataType.BFLOAT16:
+        array = array.view(ml_dtypes.bfloat16)
+    elif tensor.dtype == ir.DataType.FLOAT8E4M3FN:
+        array = array.view(ml_dtypes.float8_e4m3fn)
+    elif tensor.dtype == ir.DataType.FLOAT8E4M3FNUZ:
+        array = array.view(ml_dtypes.float8_e4m3fnuz)
+    elif tensor.dtype == ir.DataType.FLOAT8E5M2:
+        array = array.view(ml_dtypes.float8_e5m2)
+    elif tensor.dtype == ir.DataType.FLOAT8E5M2FNUZ:
+        array = array.view(ml_dtypes.float8_e5m2fnuz)
+    return array
+
+
+def display_tensor_repr(tensor: ir.TensorProtocol | None) -> str:
     if tensor is None:
         return "Data not available"
-    if tensor.size > _TENSOR_DISPLAY_LIMIT or isinstance(tensor, ir.ExternalTensor):
-        return str(tensor)
-    try:
-        array = tensor.numpy()
-        if tensor.dtype == ir.DataType.BFLOAT16:
-            array = array.view(ml_dtypes.bfloat16)
-        elif tensor.dtype == ir.DataType.FLOAT8E4M3FN:
-            array = array.view(ml_dtypes.float8_e4m3fn)
-        elif tensor.dtype == ir.DataType.FLOAT8E4M3FNUZ:
-            array = array.view(ml_dtypes.float8_e4m3fnuz)
-        elif tensor.dtype == ir.DataType.FLOAT8E5M2:
-            array = array.view(ml_dtypes.float8_e5m2)
-        elif tensor.dtype == ir.DataType.FLOAT8E5M2FNUZ:
-            array = array.view(ml_dtypes.float8_e5m2fnuz)
-        return np.array2string(array, separator=",")
-    except Exception as e:
-        logger.warning("Failed to display tensor: %s", e)
     return str(tensor)
+
+
+def can_display_tensor_json(
+    tensor: ir.TensorProtocol | None, settings: Settings
+) -> bool:
+    if tensor is None:
+        return False
+    if tensor.size > settings.const_element_count_limit:
+        return False
+    if isinstance(tensor, ir.ExternalTensor):
+        return False
+    return True
+
+
+def display_tensor_json(tensor: ir.TensorProtocol | None, settings: Settings) -> str:
+    try:
+        array = _tensor_to_numpy(tensor)
+        size_limit = settings.const_element_count_limit
+        if size_limit < 0 or size_limit > array.size:
+            # Use separators=(',', ':') to remove spaces
+            return json.dumps(array.tolist(), separators=(",", ":"))
+        # Show the first `size_limit` elements if the tensor is too large
+        return json.dumps(
+            (array.flatten())[:size_limit].tolist(), separators=(",", ":")
+        )
+    except Exception as e:
+        logger.warning("Failed to display tensor (%s): %s", tensor, e)
+    return ""
 
 
 def format_shape(shape: ir.ShapeProtocol | None) -> str:
@@ -177,11 +208,19 @@ def add_outputs_metadata(
         node.outputsMetadata.append(metadata)
 
 
-def add_node_attrs(onnx_node: ir.Node, node: graph_builder.GraphNode) -> None:
+def add_node_attrs(
+    onnx_node: ir.Node, node: graph_builder.GraphNode, settings: Settings
+) -> None:
     for attr in onnx_node.attributes.values():
         if isinstance(attr, ir.Attr):
             if attr.type == ir.AttributeType.TENSOR:
-                attr_value = display_tensor(attr.value)
+                if can_display_tensor_json(attr.value, settings=settings):
+                    set_attr(
+                        node,
+                        "__value",
+                        display_tensor_json(attr.value, settings=settings),
+                    )
+                attr_value = display_tensor_repr(attr.value)
             elif onnx_node.op_type == "Cast" and attr.name == "to":
                 attr_value = str(ir.DataType(attr.value))
             else:
@@ -249,6 +288,7 @@ def create_node(
     namespace: str,
     all_function_ids: set[ir.OperatorIdentifier],
     opset_version: int,
+    settings: Settings,
 ) -> graph_builder.GraphNode | None:
     """Create a GraphNode from an ONNX node.
 
@@ -274,7 +314,7 @@ def create_node(
         namespace=namespace,
     )
     add_incoming_edges(onnx_node, node, graph_inputs)
-    add_node_attrs(onnx_node, node)
+    add_node_attrs(onnx_node, node, settings=settings)
     add_inputs_metadata(onnx_node, node, opset_version=opset_version)
     add_outputs_metadata(onnx_node, node, opset_version=opset_version)
     if onnx_node.op_identifier() in all_function_ids:
@@ -363,6 +403,7 @@ def add_initializers(
     initializers: Sequence[ir.Value],
     namespace: str,
     all_nodes: dict[str, graph_builder.GraphNode],
+    settings: Settings,
 ) -> None:
     for initializer in initializers:
         if not initializer.name:
@@ -375,7 +416,13 @@ def add_initializers(
             # The initializer is also a graph input. Fill in the missing metadata.
             node = all_nodes[initializer_node_name]
             metadata = node.outputsMetadata[0]
-            set_attr(metadata, "value", display_tensor(initializer.const_value))
+            if can_display_tensor_json(initializer.const_value, settings=settings):
+                set_attr(
+                    node,
+                    "__value",
+                    display_tensor_json(initializer.const_value, settings=settings),
+                )
+            set_attr(metadata, "value", display_tensor_repr(initializer.const_value))
             continue
         node = graph_builder.GraphNode(
             id=initializer_node_name,
@@ -392,7 +439,13 @@ def add_initializers(
         metadata = graph_builder.MetadataItem(id="0", attrs=[])
         set_attr(metadata, "__tensor_tag", initializer.name or "")
         set_type_shape_metadata(metadata, initializer.const_value)
-        set_attr(metadata, "value", display_tensor(initializer.const_value))
+        if can_display_tensor_json(initializer.const_value, settings=settings):
+            set_attr(
+                node,
+                "__value",
+                display_tensor_json(initializer.const_value, settings=settings),
+            )
+        set_attr(metadata, "value", display_tensor_repr(initializer.const_value))
         # Note if the initializer is unused
         if not initializer.uses():
             set_attr(metadata, "unused", "True")
@@ -404,6 +457,7 @@ def create_graph(
     onnx_graph: ir.Graph | ir.Function,
     all_function_ids: set[ir.OperatorIdentifier],
     opset_version: int,
+    settings: Settings,
 ) -> graph_builder.Graph | None:
     if isinstance(onnx_graph, ir.Function):
         graph_name = get_function_graph_name(onnx_graph.identifier())
@@ -426,6 +480,7 @@ def create_graph(
             namespace=graph_name,
             all_function_ids=all_function_ids,
             opset_version=opset_version,
+            settings=settings,
         )  # type: ignore
         if node is None:
             continue
@@ -439,6 +494,7 @@ def create_graph(
             list(onnx_graph.initializers.values()),
             graph_name,
             all_nodes=all_nodes,
+            settings=settings,
         )
 
     # Add outputs
@@ -460,7 +516,7 @@ class ONNXAdapter(model_explorer.Adapter):
     def convert(
         self, model_path: str, settings: dict[str, Any]
     ) -> model_explorer.ModelExplorerGraphs:
-        del settings  # Unused
+        parsed_settings = Settings(**settings)
 
         # Do not load external data because the model file is copied to a temporary location
         # and the external data paths are not valid anymore.
@@ -489,14 +545,20 @@ class ONNXAdapter(model_explorer.Adapter):
                 model_path,
             )
         main_graph = create_graph(
-            model.graph, all_function_ids, opset_version=opset_version
+            model.graph,
+            all_function_ids,
+            opset_version=opset_version,
+            settings=parsed_settings,
         )
         assert main_graph is not None, "Bug: Main graph should not be None"
         graphs.append(main_graph)
 
         for function in model.functions.values():
             function_graph = create_graph(
-                function, all_function_ids, opset_version=opset_version
+                function,
+                all_function_ids,
+                opset_version=opset_version,
+                settings=parsed_settings,
             )
             assert function_graph is not None
             graphs.append(function_graph)
